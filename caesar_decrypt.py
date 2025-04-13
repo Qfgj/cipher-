fc3f70f63a1dc057d53a50ebb7d147e87a6c02a1
حيدر خالد <haider.asdg@gmail.com>
2025-04-14 01:34:16 +0300

    Add files via upload

---
+++ caesar_decrypt.py
@@ -0,0 +1,22 @@
+
+def caesar_decrypt(ciphertext, shift):
+    decrypted_text = ""
+    for char in ciphertext:
+        if char.isalpha():
+            shifted = ord(char) - shift
+            if char.islower():
+                if shifted < ord('a'):
+                    shifted += 26
+                decrypted_text += chr(shifted)
+            elif char.isupper():
+                if shifted < ord('A'):
+                    shifted += 26
+                decrypted_text += chr(shifted)
+        else:
+            decrypted_text += char
+    return decrypted_text
+
+ciphertext = "Khoor Zruog"
+shift = 3
+decrypted = caesar_decrypt(ciphertext, shift)
+print("Decrypted text:", decrypted)
